b2e32b425ff21ae2ef3fcbd8ca84fbd5e8bd8587
Rafael Alexander Piedra Granda <rafael.piedra@businesswarecorp.com>
2024-08-05 10:19:15 -0500

    test

--- Asignatura_DsS_2024A/settings.py
+++ Asignatura_DsS_2024A/settings.py
@@ -134,7 +134,7 @@ LOGGING = {
     'handlers': {
         'db': {
             'level': 'DEBUG',
-            'class': 'app.incident',  # Ajustar la ruta según la ubicación de logs.py
+            'class': 'app.incident.DatabaseLogHandler',  # Ajustar la ruta según la ubicación de logs.py
         },
     },
     'loggers': {
